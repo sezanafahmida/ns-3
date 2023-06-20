79797948e88800af1ae8a3980afff53b36a4957c
sezanafahmida <sezanafahmida@gmail.com>
2023-06-20 15:55:39 -0400

    Latest changes for driver scripts

--- runH.sh
+++ runH.sh
@@ -1,14 +1,19 @@
-node_num=100
-l=120 #timeslot length
+#!/bin/bash
+node_num=5
+l=60 #timeslot length
 l_min=$((l/60))
 theta=0.5
 h=1
 initD=0
 m=1;
-simDay=3
-#for theta in {0.05,0.5,1} #$(seq 0.05 0.45 0.5)
-#do
+day=2
+nw=1 #new weight
+incr=0 #new nodes
+for theta in {0.05,0.5,1} # $(seq 0.05 0.45 0.5)
+do
 #for node_num in {100,250,500}
+#do
+#for nw in {0.1,0.4,0.8}
 #do
 rm /home/gp7532/ns-3/SOC.csv
 rm /home/gp7532/ns-3/day.csv
@@ -21,11 +26,16 @@ rm /home/gp7532/ns-3/AvgError.csv
 
 #for h in {1}
 #do((day=1;day<=$simDay;day++))
-for ((day=1;day<=$simDay;day++))
-  do
+#for ((day=1;day<=$simDay;day++))
 
+for day in $(seq 1 1 2)
+  do
+  #if [ $((day%2)) -eq 0 ] ;then
+  #   node_num=$((node_num+100))
+  #   incr=100
+ # fi
   echo "$node_num, $day , $theta ,$h"
- ./waf --run "complete-network-example --nDevices=$node_num --l=$l --h=$h --day=$day --theta=$theta --b=0" >sim_run_2.txt
+ ./waf --run "complete-network-example --nDevices=$node_num --l=$l --h=$h --day=$day --theta=$theta --b=0 --nw=$nw --incr=$incr">sim_run_2.txt
   python3 Battery_degradation.py $l_min $day $node_num $initD
   ret=$?
   if [ $ret -ne 0 ]; then
@@ -34,34 +44,30 @@ for ((day=1;day<=$simDay;day++))
   if [ $((day%30)) -eq 0 ] 
   then 
      #cp /home/gp7532/ns-3/output.csv /home/gp7532/ns-3/Lifespan_heuristic/output_h_day_${day}_${node_num}.csv
-     cp /home/gp7532/ns-3/d_out.csv /home/gp7532/ns-3/Lifespan_heuristic/dout_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-     cp /home/gp7532/ns-3/GE.csv /home/gp7532/ns-3/Lifespan_heuristic/GE_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-     cp /home/gp7532/ns-3/AvgError.csv /home/gp7532/ns-3/Lifespan_heuristic/AvgError_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
+     cp /home/gp7532/ns-3/d_out.csv /home/gp7532/ns-3/Lifespan_heuristic/dout_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}.csv
+     cp /home/gp7532/ns-3/GE.csv /home/gp7532/ns-3/Lifespan_heuristic/GE_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}.csv
+    # cp /home/gp7532/ns-3/AvgError.csv /home/gp7532/ns-3/Lifespan_heuristic/AvgError_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}.csv
   
   fi
   if [ $day -eq 1 ];then
-     cp /home/gp7532/ns-3/d_out.csv /home/gp7532/ns-3/Lifespan_heuristic/dout_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
+     cp /home/gp7532/ns-3/d_out.csv /home/gp7532/ns-3/Lifespan_heuristic/dout_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}.csv
   fi
-  if [ $day -ge 180 ] && [ $day -le 360 ];then
-    theta=`echo "scale=4;${RANDOM}/32767"|bc -l`
-  fi
-  if [ $day -gt 210 ];then
-     theta=0.5
-  fi
+
+  
   rm /home/gp7532/ns-3/Trace/trace*
   done
 
-mv /home/gp7532/ns-3/output.csv /home/gp7532/ns-3/Result_h/output_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-mv /home/gp7532/ns-3/d_out.csv /home/gp7532/ns-3/Result_h/dout_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-mv /home/gp7532/ns-3/AvgLatency.csv /home/gp7532/ns-3/Result_h/AvgLatency_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-mv /home/gp7532/ns-3/AvgError.csv /home/gp7532/ns-3/Result_h/AvgError_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-mv /home/gp7532/ns-3/sim_results_0122.csv /home/gp7532/ns-3/Result_h/sim_result_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-mv /home/gp7532/ns-3/timeslot.csv /home/gp7532/ns-3/Result_h/timeslot_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-mv /home/gp7532/ns-3/receptionSlot.csv /home/gp7532/ns-3/Result_h/receptionSlot_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-mv /home/gp7532/ns-3/prr.csv /home/gp7532/ns-3/Result_h/prr_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-mv /home/gp7532/ns-3/avgR.csv /home/gp7532/ns-3/Result_h/avgR_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-mv /home/gp7532/ns-3/txE.csv /home/gp7532/ns-3/Result_h/txE_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-mv /home/gp7532/ns-3/GE.csv /home/gp7532/ns-3/Result_h/GE_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_tc6.csv
-#done
+mv /home/gp7532/ns-3/output.csv /home/gp7532/ns-3/Result_h/output_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_nw_${nw}.csv
+mv /home/gp7532/ns-3/d_out.csv /home/gp7532/ns-3/Result_h/dout_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_nw_${nw}.csv
+mv /home/gp7532/ns-3/AvgLatency.csv /home/gp7532/ns-3/Result_h/AvgLatency_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_nw_${nw}.csv
+#mv /home/gp7532/ns-3/AvgError.csv /home/gp7532/ns-3/Result_h/AvgError_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}.csv
+mv /home/gp7532/ns-3/sim_results_0122.csv /home/gp7532/ns-3/Result_h/sim_result_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_nw_${nw}.csv
+mv /home/gp7532/ns-3/timeslot.csv /home/gp7532/ns-3/Result_h/timeslot_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_nw_${nw}.csv
+mv /home/gp7532/ns-3/receptionSlot.csv /home/gp7532/ns-3/Result_h/receptionSlot_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_nw_${nw}.csv
+mv /home/gp7532/ns-3/prr.csv /home/gp7532/ns-3/Result_h/prr_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_nw_${nw}.csv
+mv /home/gp7532/ns-3/avgR.csv /home/gp7532/ns-3/Result_h/avgR_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_nw_${nw}.csv
+mv /home/gp7532/ns-3/txE.csv /home/gp7532/ns-3/Result_h/txE_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_nw_${nw}.csv
+mv /home/gp7532/ns-3/GE.csv /home/gp7532/ns-3/Result_h/GE_h_d${day}_n${node_num}_t${theta}_h${h}_i${initD}_nw_${nw}.csv
+done
 #done
 #done
--- runLora.sh
+++ runLora.sh
@@ -1,10 +1,10 @@
-node_num=100
-l=120 #timeslot length
+node_num=500
+l=60 #timeslot length
 l_min=$((l/60))
 initD=0
 h=0
-theta=0.5
-simDay=365
+theta=1
+simDay=1
 rm /home/gp7532/ns-3/SOC.csv
 rm /home/gp7532/ns-3/day.csv
 rm /home/gp7532/ns-3/Mean/mean*
@@ -12,7 +12,9 @@ rm /home/gp7532/ns-3/Trace/trace*
 rm /home/gp7532/ns-3/Prob/prob*
 rm /home/gp7532/ns-3/node_info.json
 
-for ((day=1;day<=$simDay;day++))
+#for theta in {0.05,0.5,1}
+#do 
+for day in {1..1825..1}
   do
   echo "$node_num, $day, $h"
  ./waf --run "complete-network-example --nDevices=$node_num --l=$l --h=$h --day=$day --theta=$theta" >sim_run.txt
@@ -26,14 +28,14 @@ for ((day=1;day<=$simDay;day++))
      #cp /home/gp7532/ns-3/output.csv /home/gp7532/ns-3/Lifespan_LoRa/output_lora_day_${day}_${node_num}.csv
      cp /home/gp7532/ns-3/d_out.csv /home/gp7532/ns-3/Lifespan_LoRa/dout_lora_d${day}_n${node_num}_t${theta}_i${initD}.csv
      cp /home/gp7532/ns-3/GE.csv /home/gp7532/ns-3/Lifespan_LoRa/GE_lora_d${day}_n${node_num}_t${theta}_h${h}_i${initD}.csv
-     cp /home/gp7532/ns-3/AvgError.csv /home/gp7532/ns-3/Lifespan_LoRa/AvgError_lora_d${day}_n${node_num}_t${theta}_h${h}_i${initD}.csv
+     #cp /home/gp7532/ns-3/AvgError.csv /home/gp7532/ns-3/Lifespan_LoRa/AvgError_lora_d${day}_n${node_num}_t${theta}_h${h}_i${initD}.csv
   fi
   rm /home/gp7532/ns-3/Trace/trace*
   done
 mv /home/gp7532/ns-3/output.csv /home/gp7532/ns-3/Result_Lora/output_lora_d${day}_n${node_num}_t${theta}_i${initD}.csv
 mv /home/gp7532/ns-3/d_out.csv /home/gp7532/ns-3/Result_Lora/dout_lora_d${day}_n${node_num}_t${theta}_i${initD}.csv
 mv /home/gp7532/ns-3/AvgLatency.csv /home/gp7532/ns-3/Result_Lora/AvgLatency_lora_d${day}_n${node_num}_t${theta}_i${initD}.csv
-mv /home/gp7532/ns-3/AvgError.csv /home/gp7532/ns-3/Result_Lora/AvgError_lora_d${day}_n${node_num}_t${theta}_i${initD}.csv
+#mv /home/gp7532/ns-3/AvgError.csv /home/gp7532/ns-3/Result_Lora/AvgError_lora_d${day}_n${node_num}_t${theta}_i${initD}.csv
 mv /home/gp7532/ns-3/sim_results_0122.csv /home/gp7532/ns-3/Result_Lora/sim_result_lora_d${day}_n${node_num}_t${theta}_i${initD}.csv
 mv /home/gp7532/ns-3/timeslot.csv /home/gp7532/ns-3/Result_Lora/timeslot_lora_d${day}_n${node_num}_t${theta}_i${initD}.csv
 mv /home/gp7532/ns-3/receptionSlot.csv /home/gp7532/ns-3/Result_Lora/receptionSlot_lora_d${day}_n${node_num}_t${theta}_i${initD}.csv
@@ -41,3 +43,4 @@ mv /home/gp7532/ns-3/prr.csv /home/gp7532/ns-3/Result_Lora/prr_lora_d${day}_n${n
 mv /home/gp7532/ns-3/avgR.csv /home/gp7532/ns-3/Result_Lora/avgR_lora_d${day}_n${node_num}_t${theta}_i${initD}.csv
 mv /home/gp7532/ns-3/txE.csv /home/gp7532/ns-3/Result_Lora/txE_lora_d${day}_n${node_num}_t${theta}_i${initD}.csv
 mv /home/gp7532/ns-3/GE.csv /home/gp7532/ns-3/Result_Lora/GE_lora_d${day}_n${node_num}_t${theta}_h${h}_i${initD}.csv
+#done
